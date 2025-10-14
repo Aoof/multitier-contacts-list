02d91eb5bcf53c917c764b5d86ddfee5db06df37
aoof <aoofxd@gmail.com>
2025-10-14 18:28:04 -0400

    added the sql file to show the database table structure

---
+++ MultiTierContactList_Project/Contact.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Contact] (
+    [Id]        INT           NOT NULL,
+    [FirstName] VARCHAR (50)  NOT NULL,
+    [LastName]  VARCHAR (50)  NOT NULL,
+    [Email]     VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([Email] ASC)
+);
